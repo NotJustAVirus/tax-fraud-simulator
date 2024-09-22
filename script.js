9d82f83f9545346cb6406ab4d3ceddbc4e7516dd
JustAVirus <bern64390@gmail.com>
2024-09-22 21:08:53 +0200

    Add StartMenuApp and functionality

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-$(".startmenu-button").click(function() {
-    $(".startmenu").toggleClass("hidden");
-});
-
-
-$(".app-button").click(function() {
-    let appLink = $(this).data("app");
-    appManager.launchApp(appLink);
-});
-
-
-
 class AppManager {
     focus = null;
 
@@ -17,6 +5,8 @@ class AppManager {
         this.apps = [];
         $(".startmenu").click(() => this.focusApp(null));
         // $("#desktop").click(() => this.focusApp(null));
+        let startmenu = new StartMenuApp();
+        this.addApp(startmenu);
         let browser = new App("WaterCat", "water-cat.webp", "browser");
         browser.pin();
         this.addApp(browser);
@@ -47,13 +37,13 @@ class AppManager {
             }
             // If app is open and focused, minimize it
             if (this.focus === app) {
-                app.el.addClass("minimized");
+                app.minimize();
                 this.focusApp(null);
                 return;
             }
             // If app is open but minimized, restore it
             app.el.appendTo("#desktop"); // Move to front
-            setTimeout(() => app.el.removeClass("minimized"), 1);
+            setTimeout(() => app.minimize(false), 1);
             this.focusApp(app);
         } else {
             console.error("App not found");
@@ -100,11 +90,15 @@ class App {
         this.el.find(".window-title").text(this.name);
         this.el.find(".window-icon img").attr("src", "/icon/" + this.icon);
         this.el.find(".close").click(() => this.close());
-        this.el.find(".minimize").click(() => {
+        this.el.find(".minimize").click((e) => {
+            e.stopPropagation();
             this.el.addClass("minimized");
             appManager.focusApp(null);
         });
         this.el.appendTo("#desktop"); 
+        this.el.click(() => {
+            appManager.focusApp(this)
+        });
     }
 
     createAppTaskbar() {
@@ -118,6 +112,10 @@ class App {
     focus(focused) {
         this.el.toggleClass("focused", focused);
         this.taskbar.toggleClass("focused", focused);
+    }
+
+    minimize(bool = true) {
+        this.el.toggleClass("minimized", bool);
     }
 
     pin() {
@@ -136,4 +134,19 @@ class App {
         }
     }
 }
+
+class StartMenuApp extends App {
+    constructor() {
+        super("Start", "door.png", "start")
+        this.open = true;
+        this.el = $("");
+        this.pin();
+    }
+
+    focus(focused) {
+        $(".startmenu").toggleClass("hidden", !focused);
+        this.taskbar.toggleClass("focused", focused);
+    }
+}
+
 let appManager = new AppManager();
