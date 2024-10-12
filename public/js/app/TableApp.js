ed0238718f1de5c49cefa192a89b3c0e2b4eb373
JustAVirus <bern64390@gmail.com>
2024-10-12 23:33:19 +0200

    Create files for table application

---
+++ public/js/app/TableApp.js
@@ -0,0 +1,7 @@
+import { App } from "./App.js";
+
+export class TableApp extends App {
+    constructor(appManager) {
+        super("Table", "table.webp", "table", appManager);
+    }
+}
--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,7 @@ import { AppManager } from "./AppManager.js";
 import { BrowserApp } from "./app/browser/BrowserApp.js";
 import { NoteBlockApp } from "./app/NoteBlockApp.js";
 import { FaxApp } from "./app/FaxApp.js";
+import { TableApp } from "./app/TableApp.js";
 
 
 let appManager = new AppManager();
@@ -10,10 +11,12 @@ let apps = [
     BrowserApp,
     NoteBlockApp,
     FaxApp,
+    TableApp,
 ];
 
 appManager.loadApps(apps);
 
-appManager.getApp("browser").pin();
-appManager.getApp("browser").launch();
-appManager.getApp("fax").pin();
+// appManager.getApp("browser").pin();
+// appManager.getApp("browser").launch();
+// appManager.getApp("fax").pin();
+appManager.getApp("table").launch();
