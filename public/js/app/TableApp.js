e1029511421a76cf96d3b71c64f10420e45409fc
JustAVirus <bern64390@gmail.com>
2024-10-16 21:43:18 +0200

    Add dynamic column height and width to Table app

--- public/js/app/TableApp.js
+++ public/js/app/TableApp.js
@@ -1,33 +1,62 @@
 import { App } from "./App.js";
 
 export class TableApp extends App {
+    columnWidth = 60;
+    rowHeight = 25;
+    columnExceptions = {
+        2: 100,
+    };
+    rowExceptions = {
+        2: 50,
+    };
+    
     constructor(appManager) {
         super("Table", "table.webp", "table", appManager);
     }
 
     onload() {
-        // draw table
         this.canvas = this.el.find("canvas")[0];
-        this.ctx = this.canvas.getContext("2d");
         this.canvas.width = 2000;
         this.canvas.height = 2000;
+        this.ctx = this.canvas.getContext("2d");
+        this.ctx.font = "16px Consolas";
+        this.drawTable();
+        this.writeCell(0, 0, "A1");
+    }
+    
+    drawTable() {
         this.ctx.fillStyle = "#f0f0f0";
         this.ctx.fillRect(0, 0, 40, 2000);
         this.ctx.fillRect(0, 0, 2000, 25);
         this.ctx.fillStyle = "#555";
-        for (let x = 40; x < 2000; x += 60) {
+        this.ctx.textAlign = "center";
+        let x = 40;
+        for (let i = 0; x < 2000; i++) {
             this.ctx.fillRect(x, 0, 1, 2000);
+            let width = this.columnWidth;
+            if (this.columnExceptions[i]) {
+                width = this.columnExceptions[i];
+            }
+            let char = "";
+            if (i >= 26) char += String.fromCharCode(65 + (i / 26) - 1);
+            char += String.fromCharCode(65 + (i % 26));
+            this.ctx.fillText(char, x + (width / 2), 17);
+            x += width;
         }
-        for (let y = 25; y < 2000; y += 25) {
+        
+        let y = 25;
+        for (let i = 0; y < 2000; i++) {
             this.ctx.fillRect(0, y, 2000, 1);
+            let height = this.rowHeight;
+            if (this.rowExceptions[i]) {
+                height = this.rowExceptions[i];
+            }
+            this.ctx.fillText(i + 1, 20, y + (height / 2) + 5);
+            y += height;
         }
-        this.ctx.fillStyle = "#000";
-        this.ctx.font = "16px Consolas";
-        for (let x = 65; x < 2000; x += 60) {
-            this.ctx.fillText(String.fromCharCode(65 + (x - 60) / 60), x, 15); // A-Z TODO: A-ZZ
-        }
-        for (let y = 45; y < 2000; y += 25) {
-            this.ctx.fillText(Math.floor(y / 25), 10, y);
-        }
+    }
+
+    writeCell(x, y, value) {
+        this.ctx.fillText(value, 65 + x * 60, 45 + y * 25);
     }
 }
