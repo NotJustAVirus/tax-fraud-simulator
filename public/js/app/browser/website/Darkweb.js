cb1c7bbf332a0e63d53caf69d6d1d8b5b66ea422
JustAVirus <bern64390@gmail.com>
2024-11-15 23:42:26 +0100

    Implement loading darkweb products

--- public/js/app/browser/website/Darkweb.js
+++ public/js/app/browser/website/Darkweb.js
@@ -3,7 +3,43 @@ import { WebsiteScript } from './WebsiteScript.js';
 export class Darkweb extends WebsiteScript {
 
     run() {
-        console.log("Darkweb is running");
-        
+        $.get("/darkweb").done((items) => {
+            this.items = items;
+            this.displayItems();
+        });
+    }
+
+    displayItems() {
+        let grid = this.DOM.find(".productgrid");
+        grid.empty();
+        for (let item of this.items) {
+            let itemNavButton = this.createItemNavButton(item);
+            grid.append(itemNavButton);
+        }
+    }
+
+    createItemNavButton(item) {
+        let imgSrc = item.image ? item.image : "/image/icon/battery.png";
+        let productDiv = $(`
+        <div class="product">
+            <img src="${imgSrc}" alt="product">
+            <div class="info">
+                <h2>${item.name}</h2>
+                <p>${item.description}</p>
+                <p class="good">${item.sus}ඞ</p>
+            </div>
+            <button class="buy">Buy</button>
+            <p class="cost">$${item.price}</p>
+        </div>`);
+        productDiv.find(".buy").click(() => {
+            $.post("/darkweb/buy", { productId: item.id }).done((res) => {
+                if (res.error) {
+                    alert(res.error);
+                } else {
+                    alert(res.message);
+                }
+            });
+        });
+        return productDiv;
     }
 }
