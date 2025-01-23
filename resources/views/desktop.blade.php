13bfc3e6781de450bde591ce223d01a20877e7fe
JustAVirus <bern64390@gmail.com>
2025-01-23 23:53:31 +0100

    Update css for buttons

--- resources/views/desktop.blade.php
+++ resources/views/desktop.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf_token" content="{{ csrf_token() }}" />
     <title>Desktop</title>
-    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/style.css">
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     <script defer src="js/script.js" type="module"></script>
 </head>
