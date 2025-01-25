20d28f5fb7d82d7a917595fcc1d564fc7a331fa5
JustAVirus <bern64390@gmail.com>
2025-01-25 16:29:33 +0100

    Refactor Darkweb class

--- public/js/app/browser/website/Darkweb.js
+++ public/js/app/browser/website/Darkweb.js
@@ -5,8 +5,7 @@ export class Darkweb extends WebsiteScript {
 
     run() {
         $.get("/darkweb").done((items) => {
-            this.items = items;
-            this.displayItems();
+            this.loadProducts(items);
         });
         GameMaster.getInstance().addCallback(this.updateGameState.bind(this));
         this.updateGameState(GameMaster.getInstance().getGameState());
@@ -21,38 +20,55 @@ export class Darkweb extends WebsiteScript {
         this.DOM.find(".sus").text(gameState.sus + "ඞ");
     }
 
-    displayItems() {
+    loadProducts(items) {
+        this.products = [];
+        for (let item of items) {
+            let product = new Product(item);
+            this.products.push(product);
+        }
         let grid = this.DOM.find(".productgrid");
         grid.empty();
-        for (let item of this.items) {
-            let itemNavButton = this.createItemNavButton(item);
-            grid.append(itemNavButton);
+        for (let product of this.products) {
+            grid.append(product.DOM);
         }
     }
+}
 
-    createItemNavButton(item) {
-        let imgSrc = item.image ? item.image : "icon/battery.png";
-        let productDiv = $(`
-        <div class="product">
-            <img src="image/${imgSrc}" alt="product">
+class Product {
+    constructor(data) {
+        this.id = data.id;
+        this.name = data.name;
+        this.description = data.description;
+        this.price = data.price;
+        this.sus = data.sus;
+        this.image = data.image ? data.image : "icon/battery.png"; // TODO: Change to meaningful default image
+
+        // this.level_criteria = data.level_criteria;
+        // this.modifier = data.modifier;
+
+        this.DOM = $(`<div class="product">
+            <img src="image/${this.image}" alt="product">
             <div class="info">
-                <h2>${item.name}</h2>
-                <p>${item.description}</p>
-                <p class="good">${item.sus}ඞ</p>
+                <h2>${this.name}</h2>
+                <p>${this.description}</p>
+                <p class="good">${this.sus}ඞ</p>
             </div>
             <button class="buy">Buy</button>
-            <p class="cost">$${item.price}</p>
+            <p class="cost">$${this.price}</p>
         </div>`);
-        productDiv.find(".buy").click(() => {
-            let _token = $("meta[name='csrf_token']").attr("content");
-            $.post("/darkweb/buy", { productId: item.id, _token: _token }).done((res) => {
-                alert("Purchase successful");
-                GameMaster.getInstance().updateGameState();
-            }).fail((res) => {
-                alert("Purchase failed");
-                console.error(res);
-            });
+        this.DOM.find(".buy").click(() => {
+            this.buy();
         });
-        return productDiv;
+    }
+
+    buy() {
+        let _token = $("meta[name='csrf_token']").attr("content");
+        $.post("/darkweb/buy", { productId: this.id, _token: _token }).done((res) => {
+            alert("Purchase successful");
+            GameMaster.getInstance().updateGameState();
+        }).fail((res) => {
+            alert("Purchase failed");
+            console.error(res);
+        });
     }
 }
